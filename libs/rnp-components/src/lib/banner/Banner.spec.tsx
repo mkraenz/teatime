ce6d5d51227b1088123bd41f64992244bf2157bf
Mirco Kraenz <software@kraenz.eu>
2024-12-08 18:55:26 +0100

    refactor(#8 daily-questions): Banner from comp lib

---
+++ libs/rnp-components/src/lib/banner/Banner.spec.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react-native';
+
+import Text from '../text/Text';
+import Banner from './Banner';
+
+describe('Banner', () => {
+  it('should render successfully', () => {
+    const { root, getByText } = render(
+      <Banner visible>
+        <Text>This is a banner.</Text>
+      </Banner>
+    );
+    expect(root).toBeTruthy();
+    expect(getByText('This is a banner.')).toBeTruthy();
+  });
+});
---
+++ libs/rnp-components/src/lib/banner/Banner.tsx
@@ -0,0 +1,4 @@
+import { Banner as RnpBanner } from 'react-native-paper';
+
+export const Banner = RnpBanner;
+export default Banner;
