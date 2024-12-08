ba00e9953e64aad4f6eb733af47c4414fa60aba0
Mirco Kraenz <software@kraenz.eu>
2024-12-08 09:29:22 +0100

    refactor(#8): export Button from rnp lib

--- libs/rnp-components/src/index.ts
+++ libs/rnp-components/src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/button/Button';
 export * from './lib/menu/Menu';
 export * from './lib/text';
---
+++ libs/rnp-components/src/lib/button/Button.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+import { Button } from './Button';
+
+describe('Button', () => {
+  it('should render successfully', () => {
+    const { root } = render(<Button>Hello</Button>);
+    expect(root).toBeTruthy();
+  });
+});
