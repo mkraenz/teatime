e64bc8a27bc478e98201a69f3f38ee0ff7ee8b76
Mirco Kraenz <software@kraenz.eu>
2024-12-15 16:59:29 +0100

    fix(#14): revert back to exporting RnpText as-is

--- libs/rnp-components/src/lib/text/Text.tsx
+++ libs/rnp-components/src/lib/text/Text.tsx
@@ -1,8 +1,4 @@
 import { Text as RnpText } from 'react-native-paper';
 
-// export const Text = RnpText;
-// export default Text;
-
-export const Text = (props: any) => (
-  <RnpText style={{ color: 'red' }} {...props} />
-);
+export const Text = RnpText;
+export default Text;
