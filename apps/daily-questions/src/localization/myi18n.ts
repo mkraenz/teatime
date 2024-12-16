809de1077486ce18fc536718ddef195f1d5556e4
Mirco Kraenz <software@kraenz.eu>
2024-12-16 08:23:54 +0100

    refactor(#13 daily-question): delay i18n global initialization

--- apps/daily-questions/src/localization/myi18n.ts
+++ apps/daily-questions/src/localization/myi18n.ts
@@ -3,11 +3,11 @@ import * as Localization from 'expo-localization';
 import i18next, { LanguageDetectorAsyncModule } from 'i18next';
 import { noop } from 'lodash';
 import { initReactI18next } from 'react-i18next';
-import { translations } from './translations';
+import { Language, translations } from './translations';
 
 const persistedLangStorageKey = 'daily-questions/language';
 // https://www.i18next.com/misc/creating-own-plugins#languagedetector
-const languageDetector: LanguageDetectorAsyncModule = {
+const langDetector: LanguageDetectorAsyncModule = {
   type: 'languageDetector' as const,
   async: true, // flags below detection to be async
   detect: async () => {
@@ -20,18 +20,23 @@ const languageDetector: LanguageDetectorAsyncModule = {
   },
 };
 
-i18next
-  .use(languageDetector)
-  .use(initReactI18next)
-  .init({
+export const createI18n = (
+  languageDetector: LanguageDetectorAsyncModule | null = langDetector,
+  lng?: Language | 'cimode'
+) => {
+  if (languageDetector) i18next.use(languageDetector);
+  i18next.use(initReactI18next).init({
     compatibilityJSON: 'v4',
     resources: translations,
     fallbackLng: 'en',
     // debug: true,
+    lng,
+    appendNamespaceToCIMode: true,
 
     interpolation: {
       escapeValue: false, // react already safes from xss attacks
     },
     cleanCode: true,
   });
-export const myi18n = i18next;
+  return i18next;
+};
