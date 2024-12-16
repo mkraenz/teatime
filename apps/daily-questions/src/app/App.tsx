2479df6af81426165dfadbd29bb3e83cd16169ec
Mirco Kraenz <software@kraenz.eu>
2024-12-16 08:23:54 +0100

    refactor(#13 daily-question): delay i18n global initialization

--- apps/daily-questions/src/app/App.tsx
+++ apps/daily-questions/src/app/App.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { myi18n } from '../localization/myi18n';
+import { createI18n } from '../localization/myi18n';
 import LocalNotifications from '../LocalNotifications';
 import NavApp from '../nav/NavApp';
 import { persistor, store } from '../store';
 import ThemedApp from '../theme/ThemedApp';
+
+const myI18n = createI18n();
 
 export const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemedApp>
-          <I18nextProvider i18n={myi18n}>
+          <I18nextProvider i18n={myI18n}>
             <LocalNotifications />
             <NavApp />
           </I18nextProvider>
